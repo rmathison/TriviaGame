657121296c9ae7fd66af0ab6428fd2813f28ff13
Willas88 <robertwmathison@gmail.com>
2017-08-31 06:09:02 -0600

    TriviaGame

--- javascript/triviagame.js
+++ javascript/triviagame.js
@@ -1,112 +1,110 @@
+$(document).ready(function() {
+    var quizLock = false;
 
-$(document).ready(function(){
-var quizLock = false;
 
+    var rightAnswer1 = 0;
+    var rightAnswer2 = 0;
+    var rightAnswer3 = 0;
+    var rightAnswer4 = 0;
+    var answerArray = ["1. Drogon", " 2. Nymeria", " 3. Aegon", " 4. Frostfangs"];
+    var myFunchin = function() {
+        $(location).attr('href', '../index.html')
+    };
 
-var rightAnswer1 = 0;
-var rightAnswer2 = 0;
-var rightAnswer3 = 0;
-var rightAnswer4 = 0;
-var answerArray = ["1. Drogon" , " 2. Nymeria" , " 3. Aegon" , " 4. Frostfangs"];
-var myFunchin = function() {
-  $(location).attr('href', '../index.html')
-};
+    var count = 30;
 
-var count=30;
+    var counter = setInterval(timer, 1000);
 
-var counter=setInterval(timer, 1000);
+    function timer() {
+        count = count - 1;
+        if (count <= -.0000) {
+            var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
+            $(".results").append("Time is up!");
+            $(".results").append(" You scored " + totalScore + " out of 4.");
+            clearInterval(counter);
 
-function timer()
-{
-  count=count-1;
-  if (count <= -.0000)
-  {
-    var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
-    $("#results").html("Time is up!");
-    $("#results").append( " You scored "  + totalScore + " out of 4.");
-     clearInterval(counter);
-    
-     setTimeout(fiveSeconds, 1000 * 10);
-     function fiveSeconds() {
-      $("#results").empty();
-     $("#results").html("The answers are: " + answerArray);
-       setTimeout(tenSeconds, 1000 * 5);
-     function tenSeconds() {
-     myFunchin();
-     return;
-     }
-     } 
+            setTimeout(twoSeconds, 1000 * 2);
 
-  }
+            function twoSeconds() {
+                $(".results").append(" - The answers are: " + answerArray);
+                setTimeout(threeSeconds, 1000 * 3);
 
-$("#timer").html(count);
-}
+                function threeSeconds() {
+                    myFunchin();
+                    return;
+                }
+            }
+
+        }
+
+        $("#timer").html(count + "");
+    }
 
 
 
-   $("#drogon1").on("click", function() {
-  rightAnswer1 = 1;
-  var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
-  console.log(totalScore);
-  });
+    $("#drogon1").on("click", function() {
+        rightAnswer1 = 1;
+        var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
+        console.log(totalScore);
+    });
 
- $("#viserion").on("click", function() {
-  rightAnswer1 = 0;
-  var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
-  console.log(totalScore);
-  });
-  $("#rhaegel").on("click", function() {
-  rightAnswer1 = 0;
-  var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
-  console.log(totalScore);
-  });
+    $("#viserion").on("click", function() {
+        rightAnswer1 = 0;
+        var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
+        console.log(totalScore);
+    });
+    $("#rhaegel").on("click", function() {
+        rightAnswer1 = 0;
+        var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
+        console.log(totalScore);
+    });
 
 
-$("#nymeria").on("click", function() {
-  rightAnswer2 =1;
-  var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
-  console.log(totalScore);
-});
-$("#summer").on("click", function() {
-  rightAnswer2 = 0;
-  var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
-  console.log(totalScore);
-});
-$("#shaggy").on("click", function() {
-  rightAnswer2 = 0;
-  var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
-  console.log(totalScore);
-});
-$("#aegon").on("click", function() {
-  rightAnswer3 = 1;
-  var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
-  console.log(totalScore);
-});
-$("#daeron").on("click", function() {
-  rightAnswer3 = 0;
-  var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
-  console.log(totalScore);
-});
-$("#jahaerys").on("click", function() {
-  rightAnswer3 = 0;
-  var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
-  console.log(totalScore);
-});
-$("#frostfangs").on("click", function() {
-  rightAnswer4 = 1;
-  var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
-  console.log(totalScore);
-});
-$("#thenns").on("click", function() {
-  rightAnswer4 = 0;
-  var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
-  console.log(totalScore);
-});
-$("#skirlings").on("click", function() {
-  rightAnswer4 = 0;
-  var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
-  console.log(totalScore);
-});
+    $("#nymeria").on("click", function() {
+        rightAnswer2 = 1;
+        var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
+        console.log(totalScore);
+    });
+    $("#summer").on("click", function() {
+        rightAnswer2 = 0;
+        var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
+        console.log(totalScore);
+    });
+    $("#shaggy").on("click", function() {
+        rightAnswer2 = 0;
+        var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
+        console.log(totalScore);
+    });
+    $("#aegon").on("click", function() {
+        rightAnswer3 = 1;
+        var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
+        console.log(totalScore);
+    });
+    $("#daeron").on("click", function() {
+        rightAnswer3 = 0;
+        var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
+        console.log(totalScore);
+    });
+    $("#jahaerys").on("click", function() {
+        rightAnswer3 = 0;
+        var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
+        console.log(totalScore);
+    });
+    $("#frostfangs").on("click", function() {
+        rightAnswer4 = 1;
+        var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
+        console.log(totalScore);
+    });
+    $("#thenns").on("click", function() {
+        rightAnswer4 = 0;
+        var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
+        console.log(totalScore);
+    });
+    $("#skirlings").on("click", function() {
+        rightAnswer4 = 0;
+        var totalScore = rightAnswer1 + rightAnswer2 + rightAnswer3 + rightAnswer4;
+        console.log(totalScore);
+    });
 
 
 
